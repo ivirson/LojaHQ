862f01c37e2d61f1da67d94c314043e997ac7b86
Ivirson M. Daltro <ivirson.m.daltro@avanade.com>
2020-11-09 13:48:25 -0300

    Criação do controller de Revistas e Configuração do swagger

--- Ivirson.LojaHQ/Ivirson.LojaHQ.API/Startup.cs
+++ Ivirson.LojaHQ/Ivirson.LojaHQ.API/Startup.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using Ivirson.LojaHQ.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace Ivirson.LojaHQ.API
 {
@@ -26,6 +23,34 @@ namespace Ivirson.LojaHQ.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Configurando o serviço de documentação do Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                        Title = "Loja Virtual HQ's",
+                        Version = "v1",
+                        Description = "API REST criada com o ASP.NET Core para o projeto de uma Loja Virtual",
+                        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                        {
+                            Name = "Ivirson Daltro",
+                            Url = new Uri("https://github.com/ivirson/LojaHQ-BackEnd")
+                        }
+                    });
+
+                string applicationPath =
+                    PlatformServices.Default.Application.ApplicationBasePath;
+                string applicationName =
+                    PlatformServices.Default.Application.ApplicationName;
+                string xmlDocPath =
+                    Path.Combine(applicationPath, $"{applicationName}.xml");
+
+                c.IncludeXmlComments(xmlDocPath);
+            });
+
+            services.AddScoped<DataContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +70,12 @@ namespace Ivirson.LojaHQ.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Loja Virtual de HQ's");
             });
         }
     }
