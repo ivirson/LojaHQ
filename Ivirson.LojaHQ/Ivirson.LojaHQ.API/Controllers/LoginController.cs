361fb32848a71f128a2364ed36d2b81f486f6cb5
Ivirson M. Daltro <ivirson.m.daltro@avanade.com>
2020-11-09 14:11:37 -0300

    Criação do controller de autenticação com método de login / Criação das configurações de geração do token de usuário autenticado

---
+++ Ivirson.LojaHQ/Ivirson.LojaHQ.API/Controllers/LoginController.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using Blog.API.Services;
+using Ivirson.LojaHQ.Data;
+using Ivirson.LojaHQ.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.API.Controllers
+{
+    [Route("api/login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public LoginController(DataContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Método que retorna um usuário logado e um token de autenticação
+        /// </summary>
+        /// <param name="model">Informações do Usuario a logar</param>
+        /// <returns>
+        /// Em caso de sucesso, retorna o Usuário autenticado e um token
+        /// Em caso de erro, retorna 404 (Not Found)
+        /// </returns>
+        [HttpPost]
+        public IActionResult Autenticar([FromBody] Usuario model)
+        {
+            // Recupera o usuário
+            var usuario = _context.Usuarios.FirstOrDefault(x => x.Login == model.Login && x.Senha ==  model.Senha);
+
+            // Verifica se o usuário existe
+            if (usuario == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            // Gera o Token
+            var token = TokenService.GenerateToken(usuario);
+
+            // Oculta a senha
+            usuario.Senha = "";
+
+            // Retorna os dados
+            return Ok(
+                new
+                {
+                    usuario = usuario,
+                    token = token
+                }
+            );
+        }
+    }
+}
